55ba89c9138b97159f6190b9a0db96582de92400
Michael <michaelllebowski@gmail.com>
2020-10-05 20:57:00 +0300

    Все типы переменных изменены на String для прохождения тестов

--- src/main/java/org/itstep/task01/Main.java
+++ src/main/java/org/itstep/task01/Main.java
@@ -118,15 +118,15 @@ public class Main {
                         }
                         case 3:
                         {
-                            int date;
-                            date = scanner.nextInt();
+                            String date;
+                            date = scanner.next();
                             man.setDateOfBirth(date);
                             break;
                         }
                         case 4:
                         {
-                            int phone;
-                            phone = scanner.nextInt();
+                            String phone;
+                            phone = scanner.next();
                             man.setPhone(phone);
                             break;
                         }
@@ -167,125 +167,3 @@ public class Main {
     }
 }
 
-class Human
-{
-    private String fullName;
-    private int dateOfBirth;
-    private int phone;
-    private String city;
-    private String country;
-    private String address;
-
-    Human()
-    {
-        fullName = "null";
-        dateOfBirth = 0;
-        phone = 0;
-        city = "null";
-        country = "null";
-        address = "null";
-    }
-
-    Human(String fullName, int dateOfBirth, int phone, String city, String country, String address)
-    {
-        this.fullName = fullName;
-        this.dateOfBirth = dateOfBirth;
-        this.phone = phone;
-        this.city = city;
-        this.country = country;
-        this.address = address;
-    }
-
-    public void fill()
-    {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter Name:");
-        fullName = scanner.nextLine();
-
-        System.out.print("Enter Date of Birth:");
-        dateOfBirth = scanner.nextInt();
-
-        System.out.print("Enter Phone:");
-        phone = scanner.nextInt();
-
-        System.out.print("Enter City:");
-        city = scanner.next();
-
-        System.out.print("Enter Country:");
-        country = scanner.next();
-
-        System.out.print("Enter Address:");
-        address = scanner.next();
-    }
-
-    public void print()
-    {
-        System.out.println("Name: " + fullName);
-        System.out.println("Date of Birth: " + dateOfBirth);
-        System.out.println("Phone: " + phone);
-        System.out.println("City: " + city);
-        System.out.println("Country: " + country);
-        System.out.println("Address: " + address);
-    }
-
-    public void setFullName(String fullName)
-    {
-        this.fullName = fullName;
-    }
-
-    public String getFullName()
-    {
-        return fullName;
-    }
-
-    public void setDateOfBirth(int dateOfBirth)
-    {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public int getDateOfBirth()
-    {
-        return  dateOfBirth;
-    }
-
-    public void setPhone(int phone)
-    {
-        this.phone = phone;
-    }
-
-    public int getPhone()
-    {
-        return phone;
-    }
-
-    public void setCity(String city)
-    {
-        this.city = city;
-    }
-
-    public String getCity()
-    {
-        return  city;
-    }
-
-    public void setCountry(String country)
-    {
-        this.country = country;
-    }
-
-    public String getCountry()
-    {
-        return country;
-    }
-
-    public void setAddress(String address)
-    {
-        this.address = address;
-    }
-
-    public String getAddress()
-    {
-        return address;
-    }
-}
